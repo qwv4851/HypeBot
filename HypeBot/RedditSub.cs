78c7351f4fe7e965db16c2381374c965082f51e8
Sean Reilly <qwv4851@gmail.com>
2015-05-05 23:59:28 -0700

    Added subreddit posting

---
+++ HypeBot/RedditSub.cs
@@ -0,0 +1,31 @@
+﻿using RedditSharp;
+using RedditSharp.Things;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HypeBot
+{
+    class RedditBot
+    {
+        private static string username = Program.GetConfig("redditUser");
+        private static string password = Program.GetConfig("redditPass");
+        private static string subreddit = Program.GetConfig("subreddit");
+        public static void Submit(string title, string url)
+        { 
+            try
+            {
+                var reddit = new Reddit();
+                var user = reddit.LogIn(username, password);
+                var sub = reddit.GetSubreddit(subreddit);
+                sub.SubmitPost(title, url);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
