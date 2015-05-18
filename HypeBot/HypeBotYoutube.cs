3ccdc22e565d63916c40bcd84ee6c20637b26a45
Seung Lee <shl7@uw.edu>
2015-05-18 00:11:27 -0700

    More fixings
    per qwv

--- HypeBot/HypeBotYoutube.cs
+++ HypeBot/HypeBotYoutube.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 using System.Xml;
 using Newtonsoft.Json;
@@ -16,26 +12,36 @@ namespace HypeBot
 
         public static String YoutubeTitle(string url)
         {
-            if (!url.Contains("youtube.com") && !url.Contains("youtu.be")) {
+            if (!url.Contains("youtube.com") && !url.Contains("youtu.be")) 
+            {
                 return null;
             }
             
-            string title;
             string videoID = url.Substring(url.Length - 11);
           
             string requestUrl = String.Format("https://www.googleapis.com/youtube/v3/videos?id={0}&key={1}&fields=items(snippet(title%2CpublishedAt)%2Cstatistics)&part=snippet", videoID, apiKey);
             WebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(requestUrl);
-            httpWebRequest.GetResponse();
-            HttpWebResponse httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-
-            using (StreamReader streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            try
             {
-                string json = streamReader.ReadToEnd();
-                XmlDocument doc = JsonConvert.DeserializeXmlNode(json, "root");
-                title = doc["root"]["items"]["snippet"]["title"].InnerText;
-                title += " [" + doc["root"]["items"]["snippet"]["publishedAt"].InnerText.Substring(0, 4) + "]";
+                httpWebRequest.GetResponse();
+                HttpWebResponse httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+                string title;
+                using (StreamReader streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    string json = streamReader.ReadToEnd();
+                    XmlDocument doc = JsonConvert.DeserializeXmlNode(json, "root");
+                    XmlElement xmlSnippet = doc["root"]["items"]["snippet"];
+                    title = xmlSnippet["title"].InnerText;
+                    title += " [" + xmlSnippet["publishedAt"].InnerText.Substring(0, 4) + "]";
+                }
+                return title;
             }
-            return title;
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                return null;
+            }
         }
     }
 }
