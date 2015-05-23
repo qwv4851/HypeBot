f4596aec228614ddbac440744755ab63eed4edac
Sean Reilly <qwv4851@gmail.com>
2015-05-22 19:17:10 -0700

    Removed unnecessary protocol switch
    It was messing with hipchat. Also added logging of bad youtube json.

--- HypeBot/HypeBotYoutube.cs
+++ HypeBot/HypeBotYoutube.cs
@@ -21,8 +21,7 @@ namespace HypeBot
           
             string requestUrl = String.Format("https://www.googleapis.com/youtube/v3/videos?id={0}&key={1}&fields=items(snippet(title%2CpublishedAt)%2Cstatistics)&part=snippet", videoID, apiKey);
             WebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(requestUrl);
-            ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3;
-            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+            string json = String.Empty;
 
             try
             {
@@ -33,6 +32,7 @@ namespace HypeBot
                 using (StreamReader streamReader = new StreamReader(httpResponse.GetResponseStream()))
                 {
                     string json = streamReader.ReadToEnd();
+                    json = streamReader.ReadToEnd();
                     XmlDocument doc = JsonConvert.DeserializeXmlNode(json, "root");
                     XmlElement xmlSnippet = doc["root"]["items"]["snippet"];
                     title = xmlSnippet["title"].InnerText;
@@ -43,6 +43,7 @@ namespace HypeBot
             catch (Exception e)
             {
                 Console.WriteLine(e.ToString());
+                Console.WriteLine("JSON: {0}", json);
                 return null;
             }
         }
